9c9394ed8679f3eb9fb5c80b69244da36d11d78f
william-reges <william.reges1986@gmail.com>
2025-12-22 12:13:42 -0300

    update terraform

--- elasticcache/infra/main.tf
+++ elasticcache/infra/main.tf
@@ -1,7 +1,3 @@
-resource "aws_elasticache_subnet_group" "redis_subnet_group" {
-  name       = "lab-subnet-group-redis"
-  subnet_ids = [aws_subnet.private_subnet.id]
-}
 
 # Redis Cluster
 resource "aws_elasticache_cluster" "redis" {
@@ -21,6 +17,32 @@ resource "aws_elasticache_cluster" "redis" {
   tags = {
     Name  = "lab-redis-cluster"
     label = "laboratorio-elasticache"
+  }
+}
+
+# Bastion EC2 para criar tunuel de conexão entre o pc local ao Elasticache
+resource "aws_instance" "bastion_redis_client" {
+  ami                  = "ami-0484b27e9143d1299"
+  instance_type        = "t2.micro"
+  subnet_id            = aws_subnet.private_subnet.id
+  iam_instance_profile = aws_iam_instance_profile.bastion_profile.name
+
+  vpc_security_group_ids = [
+    aws_security_group.sg-redis-bastion.id
+  ]
+  associate_public_ip_address = false
+
+  user_data = <<-EOF
+          #!/bin/bash
+          curl -O https://bootstrap.pypa.io/get-pip.py python3 get-pip.py --user
+          sleep 30
+          pip install redis
+          EOF
+
+  depends_on = [aws_elasticache_cluster.redis]
+
+  tags = {
+    Name = "lab-bastion-redis"
   }
 }
 
--- elasticcache/infra/network.tf
+++ elasticcache/infra/network.tf
@@ -1,3 +1,24 @@
+# VPC for Redis Lab
+resource "aws_vpc" "vpc-redis-lab" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name  = "vpc-redis-lab"
+    label = "laboratorio-elasticache"
+  }
+}
+
+# Private Subnet for Redis Lab
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.vpc-redis-lab.id
+  cidr_block        = "10.1.1.0/24"
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name  = "private-subnet-redis-lab"
+    label = "laboratorio-elasticache"
+  }
+}
+
 
 # Security Groups for Redis Lab
 # This file defines the security groups and rules for the Redis lab environment.
@@ -24,8 +45,8 @@ resource "aws_security_group_rule" "allow_to_redis" {
   from_port         = 6379
   to_port           = 6379
   protocol          = "tcp"
-#   self              = true
-  cidr_blocks       = [aws_vpc.vpc-redis-lab.cidr_block]
+  #   self              = true
+  cidr_blocks = [aws_vpc.vpc-redis-lab.cidr_block]
 
 }
 
@@ -38,7 +59,7 @@ resource "aws_security_group" "sg-redis-bastion" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks =  ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -52,6 +73,13 @@ resource "aws_security_group_rule" "allow_to_bastion_redis" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       =  ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
 
 }
+
+# Subnet Group for Redis Lab
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = "lab-subnet-group-redis"
+  subnet_ids = [aws_subnet.private_subnet.id]
+}
+
