30ac8eb18c8a20928a998d5135b640439de4c88e
william-reges <william.reges1986@gmail.com>
2026-01-23 13:11:46 -0300

    update lab-002-bastion

--- lab-002-bastion/01-ssh-to-private-ec2/infra/sg.tf
+++ lab-002-bastion/01-ssh-to-private-ec2/infra/sg.tf
@@ -44,13 +44,23 @@ resource "aws_security_group" "server-sg" {
   }
 }
 
-resource "aws_security_group_rule" "allow_ssh_fron_bastion" {
+resource "aws_security_group_rule" "allow_ssh_to_server" {
   security_group_id        = aws_security_group.server-sg.id
   type                     = "ingress"
   from_port                = 22
   to_port                  = 22
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.bastion-sg.id
+}
+
+resource "aws_security_group_rule" "allow_all_from_server" {
+  security_group_id = aws_security_group.server-sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
 }
 
 
